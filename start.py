89e3bee525f42a94291a1ea5b698e88ab8931883
AMBOT-v1 <134328176+AMBOT-v1@users.noreply.github.com>
2023-09-03 17:24:12 +0530

    Update and rename start to start.py

---
+++ start.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("git clone https://github.com/AMBOT-v1/profilepandit && cd profilepandit && python3 bot.py")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
