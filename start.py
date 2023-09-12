3bd3f8ac440466e68f212ea3e1b2285016efa685
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-09-12 07:20:45 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/AbhiMod/profilepandit && cd profilepandit && python3 bot.py")
+system ("cd && cd && rm -rf profilepandit &&git clone https://github.com/AbhiMod/profilepandit && cd profilepandit && pip install -r requirements.txt && python3 bot.py")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
