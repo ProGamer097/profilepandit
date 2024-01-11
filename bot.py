f85af270c382c075d7a53ef170befbcbe4483b49
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-01-12 01:37:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,16 @@ from datetime import datetime
 import pytz
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import asyncio
-
+import random
+from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from pyrogram.types import (
+    Message,
+    CallbackQuery,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 scheduler = AsyncIOScheduler()
 
 
@@ -19,8 +28,15 @@ logger.setLevel(logging.INFO)
 
 
 # Set up a Pyrogram bot object
-app = pyrogram.Client("ProfilePundit", bot_token="6215758518:AAHy892YGE7Sd3dWV3Oc2sELM4PaDOXkJsE", api_id=12227067, api_hash="b463bedd791aa733ae2297e6520302fe")
+app = pyrogram.Client("ProfilePundit", bot_token="6215758518:AAHCNMrXlcSGfe00mHiB0JKfAZdTGqpZryc", api_id=12227067, api_hash="b463bedd791aa733ae2297e6520302fe")
+MONGO_DB_URI = "mongodb+srv://yonerobot:kushal55@pundit.yjfpa8v.mongodb.net/?retryWrites=true&w=majority"
+mongo = MongoCli(MONGO_DB_URI)
 
+db = mongo.chats
+db = db.chatsdb
+#User 
+db = mongo.users
+db = db.users
 
 # Set up a MongoDB client object and connect to the database
 client = MongoClient("mongodb+srv://yonerobot:kushal55@pundit.yjfpa8v.mongodb.net/?retryWrites=true&w=majority")
@@ -43,6 +59,15 @@ HELP_TEXT = (
     
 )
 
+
+AM_PIC = [
+    "https://graph.org/file/98e87a02d3fffdaef2fd5.jpg",
+    "https://graph.org/file/98e87a02d3fffdaef2fd5.jpg",
+    "https://graph.org/file/98e87a02d3fffdaef2fd5.jpg",
+    "https://graph.org/file/98e87a02d3fffdaef2fd5.jpg"
+]
+
+
 def get_target_user_id(message):
     if message.reply_to_message and message.reply_to_message.from_user:
         return message.reply_to_message.from_user.id
@@ -58,6 +83,150 @@ def get_target_user_id(message):
         else:
             return int(target)
     return None
+
+#CHATGROUP
+
+async def get_chats():
+  chat_list = []
+  async for chat in db.chats.find({"chat": {"$lt": 0}}):
+    chat_list.append(chat['chat'])
+  return chat_list
+
+async def get_chat(chat):
+  chats = await get_chats()
+  if chat in chats:
+    return True
+  else:
+    return False
+
+async def add_chat(chat):
+  chats = await get_chats()
+  if chat in chats:
+    return
+  else:
+    await db.chats.insert_one({"chat": chat})
+
+async def del_chat(chat):
+  chats = await get_chats()
+  if not chat in chats:
+    return
+  else:
+    await db.chats.delete_one({"chat": chat})
+
+#USERS
+async def get_users():
+  user_list = []
+  async for user in db.users.find({"user": {"$gt": 0}}):
+    user_list.append(user['user'])
+  return user_list
+
+
+async def get_user(user):
+  users = await get_users()
+  if user in users:
+    return True
+  else:
+    return False
+
+async def add_user(user):
+  users = await get_users()
+  if user in users:
+    return
+  else:
+    await db.users.insert_one({"user": user})
+
+
+async def del_user(user):
+  users = await get_users()
+  if not user in users:
+    return
+  else:
+    await db.users.delete_one({"user": user})
+      
+      #GCAST
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : blocked the bot\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
+        
+@app.on_message(filters.command("gcast") & filters.user(OWNER_ID))
+async def broadcast(_, message):
+    if not message.reply_to_message:
+        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ ɪᴛ.")
+        return    
+    exmsg = await message.reply_text("sᴛᴀʀᴛᴇᴅ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ!")
+    all_chats = (await get_chats()) or {}
+    all_users = (await get_users()) or {}
+    done_chats = 0
+    done_users = 0
+    failed_chats = 0
+    failed_users = 0
+    for chat in all_chats:
+        try:
+            await send_msg(chat, message.reply_to_message)
+            done_chats += 1
+            await asyncio.sleep(0.1)
+        except Exception:
+            pass
+            failed_chats += 1
+
+    for user in all_users:
+        try:
+            await send_msg(user, message.reply_to_message)
+            done_users += 1
+            await asyncio.sleep(0.1)
+        except Exception:
+            pass
+            failed_users += 1
+    if failed_users == 0 and failed_chats == 0:
+        await exmsg.edit_text(
+            f"**sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ✅**\n\n**sᴇɴᴛ ᴍᴇssᴀɢᴇ ᴛᴏ** `{done_chats}` **ᴄʜᴀᴛs ᴀɴᴅ** `{done_users}` **ᴜsᴇʀs**",
+        )
+    else:
+        await exmsg.edit_text(
+            f"**sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ✅**\n\n**sᴇɴᴛ ᴍᴇssᴀɢᴇ ᴛᴏ** `{done_chats}` **ᴄʜᴀᴛs** `{done_users}` **ᴜsᴇʀs**\n\n**ɴᴏᴛᴇ:-** `ᴅᴜᴇ ᴛᴏ sᴏᴍᴇ ɪssᴜᴇ ᴄᴀɴ'ᴛ ᴀʙʟᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ` `{failed_users}` **ᴜsᴇʀs ᴀɴᴅ** `{failed_chats}` **ᴄʜᴀᴛs**",
+        )
+@app.on_message(group=10)
+async def chat_watcher_func(_, message):
+    try:
+        if message.from_user:
+            us_in_db = await get_user(message.from_user.id)
+            if not us_in_db:
+                await add_user(message.from_user.id)
+
+        chat_id = (message.chat.id if message.chat.id != message.from_user.id else None)
+
+        if not chat_id:
+            return
+
+        in_db = await get_chat(chat_id)
+        if not in_db:
+            await add_chat(chat_id)
+    except:
+        pass
+        
+@app.on_message(filters.command(["stats"], [".", "!", "/"]))
+async def stats(client, message):
+    users = len(await get_users())
+    chats = len(await get_chats())
+    await message.reply_photo(
+        photo=random.choice(AM_PIC),
+        caption=f"""**ᴛᴏᴛᴀʟ sᴛᴀᴛs :
+
+➻ ᴄʜᴀᴛs : {chats}
+➻ ᴜsᴇʀs : {users}
+"""
+    )
     
 @app.on_message(filters.command("broadcast") & filters.user(OWNER_ID))
 async def broadcast_command(client, message):
